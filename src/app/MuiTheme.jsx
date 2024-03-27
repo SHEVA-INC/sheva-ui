5c45c1ea922ffe210b2c82152b799c328e55b4b3
sofi tymechko <sofiatymechko@gmail.com>
2024-03-27 12:45:38 +0200

    override global theme styles

--- src/app/MuiTheme.jsx
+++ src/app/MuiTheme.jsx
@@ -7,21 +7,65 @@ const theme = createTheme({
       main: "#51379B",
       contrastText: "#ffffff",
     },
+    secondary: {
+      main: "#000000",
+      light: "rgba(0, 0, 0, 0.23)",
+    },
   },
   typography: {
     fontFamily: "Gilroy-Regular",
   },
   spacing: 4,
   shape: {
-    borderRadius: 0,
-    containerBorderRadius: 4,
+    borderRadius: 8,
+    containerBorderRadius: 1,
   },
   components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        "@global": {
+          "*": {
+            boxSizing: "border-box",
+            padding: 0,
+            margin: 0,
+          },
+        },
+      },
+    },
     MuiButtonBase: {
       styleOverrides: {
         root: {
           padding: "0px",
         },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          width: "fit-content",
+          height: "fit-content",
+          borderRadius: 0,
+          textTransform: "none",
+          fontWeight: "bold",
+        },
+      },
+      defaultProps: {
+        disableElevation: true,
+      },
+    },
+    MuiIconButton: {
+      styleOverrides: {
+        root: {
+          padding: 0,
+        },
+      },
+      defaultProps: {
+        disableRipple: true,
+      },
+    },
+    MuiCheckbox: {
+      defaultProps: {
+        disableRipple: true,
       },
     },
   },
