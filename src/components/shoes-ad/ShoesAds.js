7ae34b80038d9bf27faf6ebaea544a069cfe6aee
sofi tymechko <sofiatymechko@gmail.com>
2024-04-11 17:56:42 +0300

    add shoes ads

---
+++ src/components/shoes-ad/ShoesAds.js
@@ -0,0 +1,18 @@
+import ShoesAd from "../../assets/shoes-ad/shoes-ad.png";
+
+const ShoesAds = [
+  {
+    id: 1,
+    name: "Nike Air Max",
+    text: "Get Amazing & Great Nike Shoes",
+    image: ShoesAd,
+  },
+  {
+    id: 2,
+    name: "Nike Air Max",
+    text: "Get Amazing & Great Nike Shoes",
+    image: ShoesAd,
+  },
+];
+
+export default ShoesAds;
