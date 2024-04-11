f7352f40b8f2205b1206b4e144b8aaf161260a32
sofi tymechko <sofiatymechko@gmail.com>
2024-04-11 17:55:14 +0300

    add numbers block

---
+++ src/components/NumbersBlock.jsx
@@ -0,0 +1,55 @@
+import { Stack, Typography } from "@mui/material";
+
+const numbers = [
+  {
+    id: 1,
+    number: 1068,
+    text: "Задоволених покупців!",
+  },
+  {
+    id: 2,
+    number: 1068,
+    text: "Задоволених покупців!",
+  },
+  {
+    id: 3,
+    number: 1068,
+    text: "Задоволених покупців!",
+  },
+];
+
+const NumbersBlock = () => {
+  return (
+    <Stack
+      width={1}
+      sx={{ background: (theme) => theme.palette.primary.main }}
+      alignItems="center"
+    >
+      <Stack
+        maxWidth="xl"
+        flexDirection="row"
+        justifyContent="space-between"
+        width={1}
+        px={10}
+        py={22}
+      >
+        {numbers.map((numberItem) => (
+          <Stack key={numberItem.id}>
+            <Typography variant="h1" color="thirdly.main" fontWeight="bold">
+              {numberItem.number}
+            </Typography>
+            <Typography
+              color="thirdly.main"
+              textAlign="center"
+              fontWeight="bold"
+            >
+              {numberItem.text}
+            </Typography>
+          </Stack>
+        ))}
+      </Stack>
+    </Stack>
+  );
+};
+
+export default NumbersBlock;
