d96bed728d92cb4333c7431632a955357e6a2f4c
sofi tymechko <sofiatymechko@gmail.com>
2024-04-20 21:52:19 +0300

    make ShoesCarousel responsive, adding function to group ShoesCarouselData into arrays regarding the screen size

--- src/components/shoes-slider/ShoesCarousel.jsx
+++ src/components/shoes-slider/ShoesCarousel.jsx
@@ -1,17 +1,73 @@
 import Carousel from "react-material-ui-carousel";
 import ShoesCarouselItem from "./ShoesCarouselItem";
-import { Grid, Stack } from "@mui/material";
+import { Grid, Stack, useMediaQuery, useTheme } from "@mui/material";
 import StyledTitle from "../styled/StyledTitle";
+import { useState, useEffect } from "react";
+
+const gridItemSize = {
+  xs: 12,
+  sm: 6,
+  md: 4,
+};
+
+const amountOfItemsPerScreenSize = {
+  xs: 1,
+  sm: 2,
+  md: 3,
+};
 
 const ShoesCarousel = ({ title, shoesCarouselData, id }) => {
+  const theme = useTheme();
+  const [screenSize, setScreenSize] = useState();
+  const isXs = useMediaQuery(theme.breakpoints.only("xs"));
+  const isSm = useMediaQuery(theme.breakpoints.only("sm"));
+  const isMd = useMediaQuery(theme.breakpoints.up("md"));
+
+  useEffect(() => {
+    if (isXs) {
+      setScreenSize("xs");
+    } else if (isSm) {
+      setScreenSize("sm");
+    } else if (isMd) {
+      setScreenSize("md");
+    }
+  }, [isXs, isSm, isMd]);
+
+  function groupArrayByAmountOfItemsPerScreenSize(array, screenSize) {
+    const chunks = [];
+    let chunkSize = amountOfItemsPerScreenSize.xs;
+
+    if (screenSize === "xs") {
+      chunkSize = amountOfItemsPerScreenSize.xs;
+    } else if (screenSize === "sm") {
+      chunkSize = amountOfItemsPerScreenSize.sm;
+    } else if (screenSize === "md") {
+      chunkSize = amountOfItemsPerScreenSize.md;
+    }
+
+    for (let i = 0; i < array.length; i += chunkSize) {
+      chunks.push(array.slice(i, i + chunkSize));
+    }
+    return chunks;
+  }
+
   return (
-    <Stack gap={8} id={id}>
+    <Stack gap={6} id={id}>
       <StyledTitle title={title} />
-      <Carousel autoPlay={true} cycleNavigation>
-        {shoesCarouselData.map((shoesItems, idx) => (
-          <Grid container spacing={8} key={idx}>
+      <Carousel autoPlay={true} cycleNavigation swipe navButtonsAlwaysVisible>
+        {groupArrayByAmountOfItemsPerScreenSize(
+          shoesCarouselData,
+          screenSize,
+        ).map((shoesItems, idx) => (
+          <Grid container spacing={6} key={idx}>
             {shoesItems.map((shoesItem) => (
-              <Grid item xs={4} key={shoesItem.id}>
+              <Grid
+                item
+                xs={gridItemSize.xs}
+                sm={gridItemSize.sm}
+                md={gridItemSize.md}
+                key={shoesItem.id}
+              >
                 <ShoesCarouselItem
                   key={shoesItem.id}
                   name={shoesItem.name}
