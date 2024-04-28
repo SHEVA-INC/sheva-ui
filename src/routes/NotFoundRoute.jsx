20c3101e75b1f1346f74cb0282153e50fe8dde79
sofi tymechko <sofiatymechko@gmail.com>
2024-04-28 18:37:05 +0300

    wrap NotFoundRoute into StyledStackForRoutes

--- src/routes/NotFoundRoute.jsx
+++ src/routes/NotFoundRoute.jsx
@@ -1,7 +1,12 @@
 import NotFoundBlock from "../components/NotFoundBlock";
+import StyledStackForRoutes from "../components/styled/StyledStackForRoutes";
 
 const NotFoundRoute = () => {
-  return <NotFoundBlock />;
+  return (
+    <StyledStackForRoutes alignItems="center">
+      <NotFoundBlock />
+    </StyledStackForRoutes>
+  );
 };
 
 export default NotFoundRoute;
