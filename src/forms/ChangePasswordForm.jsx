0f48f8e1e9e328429d79994312c0a3e014197c66
sofi tymechko <sofiatymechko@gmail.com>
2024-06-18 16:42:55 +0300

    implement change password functionality

--- src/forms/ChangePasswordForm.jsx
+++ src/forms/ChangePasswordForm.jsx
@@ -1,21 +1,36 @@
 import { Button, Typography } from "@mui/material";
 import StyledForm from "../components/styled/StyledForm";
 import StyledFormControlWithTextFieldForPassword from "../components/styled/StyledFormControlWithTextFieldForPassword";
-import { useState } from "react";
+import { useForm } from "react-hook-form";
+import userService from "../services/UserService";
 
 const ChangePasswordForm = () => {
-  const [currentPassword, setCurrentPassword] = useState("");
-  const [newPassword, setNewPassword] = useState("");
-  const [confirmNewPassword, setConfirmNewPassword] = useState("");
+  const {
+    register,
+    handleSubmit,
+    setError,
+    setValue,
+    getValues,
+    formState: { errors },
+  } = useForm({ mode: "all" });
 
-  const handleCurrentPasswordChange = (event) => {
-    setCurrentPassword(event.target.value);
-  };
-  const handleNewPasswordChange = (event) => {
-    setNewPassword(event.target.value);
-  };
-  const handleConfirmNewPasswordChange = (event) => {
-    setConfirmNewPassword(event.target.value);
+  const onSubmit = async (data) => {
+    const passwords = {
+      old_password: data.currentPassword,
+      new_password: data.newPassword,
+    };
+
+    try {
+      await userService.changePassword(passwords);
+      setValue("currentPassword", "");
+      setValue("newPassword", "");
+    } catch (err) {
+      console.log(err);
+      setError("currentPassword", {
+        type: "invalidCredentials",
+        message: "Неправильний поточний пароль!",
+      });
+    }
   };
 
   return (
@@ -25,26 +40,71 @@ const ChangePasswordForm = () => {
       py={6}
       boxShadow="none"
       borderRadius={0}
+      onSubmit={handleSubmit(onSubmit)}
     >
       <StyledFormControlWithTextFieldForPassword
         title="Поточний пароль"
-        value={currentPassword}
-        onChange={(e) => handleCurrentPasswordChange(e)}
-        autoComplete="current-password"
+        htmlFor="currentPassword"
+        register={{
+          ...register("currentPassword", {
+            required: {
+              value: true,
+              message: "Field is required!",
+            },
+            minLength: {
+              value: 8,
+              message: "Min length is 8 symbols!",
+            },
+            maxLength: {
+              value: 32,
+              message: "Max length is 32 symbols!",
+            },
+            pattern: {
+              value:
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[a-zA-Z\d\W_]+$/,
+              message:
+                "Enter a valid password! Password should contain at least one capital letter, one small letter, one digit, and one special symbol",
+            },
+          }),
+        }}
+        helperText={
+          errors?.currentPassword ? errors.currentPassword.message : " "
+        }
+        error={!!errors?.currentPassword}
       />
       <StyledFormControlWithTextFieldForPassword
         title="Новий пароль"
-        value={newPassword}
-        onChange={(e) => handleNewPasswordChange(e)}
-        autoComplete="new-password"
+        htmlFor="newPassword"
+        register={{
+          ...register("newPassword", {
+            required: {
+              value: true,
+              message: "Field is required!",
+            },
+            minLength: {
+              value: 8,
+              message: "Min length is 8 symbols!",
+            },
+            maxLength: {
+              value: 32,
+              message: "Max length is 32 symbols!",
+            },
+            pattern: {
+              value:
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[a-zA-Z\d\W_]+$/,
+              message:
+                "Enter a valid password! Password should contain at least one capital letter, one small letter, one digit, and one special symbol",
+            },
+            validate: (value) =>
+              value !== getValues("currentPassword") ||
+              "Поточний пароль та новий мають бути різними!!",
+          }),
+        }}
+        helperText={errors?.newPassword ? errors.newPassword.message : " "}
+        error={!!errors?.newPassword}
       />
-      <StyledFormControlWithTextFieldForPassword
-        title="Підтвердити пароль"
-        value={confirmNewPassword}
-        onChange={(e) => handleConfirmNewPasswordChange(e)}
-        autoComplete="new-password"
-      />
-      <Button variant="contained" size="large">
+
+      <Button variant="contained" size="large" type="submit">
         <Typography variant="body2" textTransform="uppercase" fontWeight="bold">
           Змінити пароль
         </Typography>
