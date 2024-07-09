c0e9c772ad3dd5549e53b5d1ac507d2014cf6a7a
sofi tymechko <sofiatymechko@gmail.com>
2024-07-09 14:13:14 +0300

    add .env with variables for prod

--- src/api/AxiosInterceptor.jsx
+++ src/api/AxiosInterceptor.jsx
@@ -3,8 +3,10 @@ import { useEffect, useState } from "react";
 import AuthManager from "../auth/TokenService";
 import useAuth from "../auth/useAuth";
 
+const baseUrl = import.meta.env.VITE_BASE_URL;
+
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: baseUrl,
 });
 
 axiosInstance.interceptors.request.use(
