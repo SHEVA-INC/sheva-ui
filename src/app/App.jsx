f6301d3d6305dc280f3293c0a4febce1990c8bf2
sofi tymechko <sofiatymechko@gmail.com>
2024-08-29 01:23:40 +0300

    add cookie consent snackbar

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,15 +3,22 @@ import MuiTheme from "./MuiTheme";
 import Router from "./Router";
 import { AuthProvider } from "../auth/useAuth";
 import AxiosInterceptor from "../api/AxiosInterceptor";
+import CookieConsentSnackbar from "../components/CookieConsentSnackbar";
+import { CookiesProvider, useCookies } from "react-cookie";
 
 const App = () => {
+  const [cookies] = useCookies(["cookieConsent"]);
+
   return (
     <MuiTheme>
       <AuthProvider>
         <AxiosInterceptor>
-          <BrowserRouter>
-            <Router />
-          </BrowserRouter>
+          <CookiesProvider defaultSetOptions={{ path: "/" }}>
+            <BrowserRouter>
+              <Router />
+              {!cookies.cookieConsent && <CookieConsentSnackbar />}
+            </BrowserRouter>
+          </CookiesProvider>
         </AxiosInterceptor>
       </AuthProvider>
     </MuiTheme>
